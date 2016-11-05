c7c926bae5ad15cac28b813825231a252c0d9695
rpagett <riley.pagett@gmail.com>
2016-11-05 01:16:49 -0400

    adjustments

--- bin/models/Form/FormModals.js
+++ bin/models/Form/FormModals.js
@@ -13,6 +13,8 @@ var _react2 = _interopRequireDefault(_react);
 
 var _reactRedux = require('react-redux');
 
+var _reactRouter = require('react-router');
+
 var _isomorphicFetch = require('isomorphic-fetch');
 
 var _isomorphicFetch2 = _interopRequireDefault(_isomorphicFetch);
@@ -299,7 +301,7 @@ var _SubmitForm = function (_React$Component2) {
   return _SubmitForm;
 }(_react2.default.Component);
 
-var SubmitForm = exports.SubmitForm = withRouter((0, _functions.authConnect)(_SubmitForm));
+var SubmitForm = exports.SubmitForm = (0, _reactRouter.withRouter)((0, _functions.authConnect)(_SubmitForm));
 
 var AssignObligation = exports.AssignObligation = function (_React$Component3) {
   _inherits(AssignObligation, _React$Component3);
--- src/models/Form/FormModals.jsx
+++ src/models/Form/FormModals.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { connect } from 'react-redux';
+import { withRouter } from 'react-router';
 
 import fetch from 'isomorphic-fetch';
 import LoadingCube from '../../helpers/LoadingCube';
